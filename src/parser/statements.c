1acdca264263dadfb63f27c7c25032e1bc065a0d
GabrielM-512 <gabriel.maurer@gmx.de>
2026-06-11 23:12:07 +0200

    Include Local Variable Declaration Statement in parseStmt() and use that in blockStmt()

--- src/parser/statements.c
+++ src/parser/statements.c
@@ -73,10 +73,8 @@ StmtNode *blockStmt(Parser *parser) {
 
     while (!match(parser, TOKEN_RIGHT_BRACE)) {
         if (match(parser, TOKEN_EOF)) parseError(parser, "Unterminated block");
-        StmtNode *next;
+        StmtNode *next = parseStmt(parser);
 
-        if (isTypeIdent(parser)) next = localVarDeclStmt(parser);
-        else next = parseStmt(parser);
         ArrayListAdd(node->content, &next);
     }
 
@@ -88,7 +86,9 @@ StmtNode *blockStmt(Parser *parser) {
 
 StmtNode *parseStmt(Parser *parser) {
     StmtNode* node;
-    if (match(parser, TOKEN_LEFT_BRACE)) node = blockStmt(parser);
+
+    if (isTypeIdent(parser)) node = localVarDeclStmt(parser);
+    else if (match(parser, TOKEN_LEFT_BRACE)) node = blockStmt(parser);
     else node = exprStmt(parser);
 
     if (parser->panicMode) synchronise(parser);
